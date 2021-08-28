d5d8b7f80af30d4dcee4675229fefc5a4e0e1cad
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-08-28 19:56:31 -0400

    Complete project

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,12 @@
 def substrings(word, dictionary)
-  
+  list = Hash.new(0)
+  word.downcase!
+  dictionary.each do |str| 
+    arr = word.scan(str)
+    list[str] = arr.size unless arr.empty?
+  end
+  list
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+print substrings("Howdy partner, sit down! How's it going?", dictionary)
