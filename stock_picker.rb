1fc51f411288e8822bf7596c5edfacc781f261d6
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-08-29 17:37:29 -0400

    Complete project

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,16 +3,18 @@ def stock_picker(stocks)
   diff = 0
   largest = 0
   result = []
+
   arr.each_with_index do |v, i|
     max = arr.max
     diff = max - v
     if diff > largest
       largest = diff
-      result =[i, arr.index(max)]
+      result = [i, arr.index(max)]
     else
       arr[i] = 0
     end
   end
+
   result
 end
 
