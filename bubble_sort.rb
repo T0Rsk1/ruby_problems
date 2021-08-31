f683131185d2962c0e09f17072878f479038e3b5
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-08-31 18:22:38 -0400

    Complete project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,20 @@
 def bubble_sort(arr)
   last_v = arr[0]
-  t = 0
-
-  until t == arr.size do
-    t = 0
-
+  t = arr.size
+  until t == 0 do
     arr.each_with_index do |v, i|
+      break if t == i
       if v >= last_v
         last_v = v
-        t += 1
       else
         arr[i-1] = v
         arr[i] = last_v
       end
     end
-    
+    t -= 1
     last_v = arr[0]
   end
-
   arr
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2)
